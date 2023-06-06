b91028c5f7f52e0b78be5a13a13e90caa77b7cd7
Gurpreet singh <33153390+gurpreet26@users.noreply.github.com>
2023-06-06 14:08:49 -0400

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const colors = require("colors");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://guri:1234@cluster0.lx8ll78.mongodb.net/");
+    await mongoose.connect(process.env.MONGO_URL);
     console.log(`Mongodb connected ${mongoose.connection.host}`.bgGreen.white);
   } catch (error) {
     console.log(`Mongodb Server Issue ${error}`.bgRed.white);
